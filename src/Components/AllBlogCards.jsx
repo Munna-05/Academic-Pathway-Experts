72f723e7aebc76845aada4fd153d1583baaa23d4
Munna <sajeevmunna05@gmail.com>
2023-11-29 01:13:05 +0530

    blog cards update

--- src/Components/AllBlogCards.jsx
+++ src/Components/AllBlogCards.jsx
@@ -8,16 +8,16 @@ const AllBlogCards = () => {
 
       <div className="bg-[#EEF5FF] ">
 
+          {/* main section  */}
 
         <div className="w-3/4 mx-auto ">
-          {/* main section  */}
 
           <div className="flex  justify-center items-center py-20">
             <div className=" flex justify-between">
 
             <div className="rounded-lg shadow-lg  shadow-stone-300 overflow-hidden">
               <img
-              className="w-full h-full object-cover"
+              className="w-[90vw] h-full object-cover"
                 src="https://zone-ui.vercel.app/assets/images/course/course_5.jpg"
                 alt=""
               />
@@ -31,11 +31,11 @@ const AllBlogCards = () => {
               <p className="text-4xl text-left font-bold hover:underline cursor-pointer mt-2">
                 The Future of Artificial Intelligence: Trends and Insights
               </p>
-              <p className="w-3/4 text-stone-500 text-sm mt-2">Nihil ea sunt facilis praesentium atque. Ab animi alias sequi molestias aut velit ea. Sed possimus eos. Et est aliquid est voluptatem.</p>
+              <p className="w-3/4 text-stone-500 text-sm line-clamp-4 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate esse, tenetur dolor in quisquam laborum doloribus ut possimus similique labore. Nihil ea sunt facilis praesentium atque. Ab animi alias sequi molestias aut velit ea. Sed possimus eos. Et est aliquid est voluptatem.</p>
 
-              <div className="absolute bottom-0 py-2 flex items-center gap-6  w-96">
+              <div className="absolute bottom-0 p-8 flex items-center gap-6  w-96">
                 <img className="h-10 object-fill shadow scale-125 rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrTzg-bVaGG0YjadE4mLt4bbacyPXB3uvcLidFs-zPlbkM9Fc6br8qpcCtN8QuOSmGaKA&usqp=CAU" alt="" />
-                <div className="text-sm font-semibold  text-stone-700">Auther Name</div>
+                <div className="text-sm font-semibold  text-stone-700">Author Name</div>
               </div>
 
             </div>
